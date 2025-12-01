74bbd3f000267bb2788b2d53e9c17ac0879ee78d
Kien Vu <vukien@gmail.com>
2025-12-02 00:43:51 +0700

    fix connectdb.php - clear .htaccess

--- connectdb.php
+++ connectdb.php
@@ -1,17 +1,14 @@
-<?php
-
 $servername = "localhost";
-$username = "toannt";
-$password = "2Gd3pT29";
+$username = "root";
+$password = "";
 $dbname = "dna";
 
-// Tạo kết nối
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
 }
+
 if (!$conn->set_charset("utf8mb4")) {
     echo "Lỗi khi thiết lập charset: " . $conn->error;
 }
